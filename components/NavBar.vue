e13dc1db7d7974ea7e0859c937ca2f5709164943
Dominique <dominique.kambale-baraka@epitech.eu>
2024-07-24 06:34:36 +0200

    encore plus beau

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,51 +3,37 @@
     <v-toolbar
         class="rounded-xl"
         title="VallureCosmectics"
-        height="150"
-        flat
+        height="350"
+        color="#FFFBFA"
         fixed
+        elevation="7"
+        extended
       >
        
+        <v-col>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-toolbar-side-icon>
+              <v-img class="logo" src="/banniere.JPG" alt="Better Voxel Logo" width="200" height="200" />
+            </v-toolbar-side-icon>
+            <v-spacer></v-spacer>
+            <v-app-bar-nav-icon style="margin-top: 7%;"></v-app-bar-nav-icon>
+          </v-row>
+          
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-card-title class="title3">WELCOME TO V'S ALLURE</v-card-title>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-card-subtitle class="title4">Unveil your radiance, embrace your allure</v-card-subtitle>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-spacer></v-spacer>
+        </v-col>
         
-        <v-spacer></v-spacer>
-        <v-toolbar-side-icon>
-          <v-img class="logo" src="/banniere.JPG" alt="Better Voxel Logo" width="200" height="200" />
-        </v-toolbar-side-icon>
-        <v-spacer></v-spacer>
-        <template v-if="!isLoggedIn">
-          <v-col cols="auto">
-            <v-btn @click="$router.push('/login')" rounded="xl"  color="#0074E6" class="title2">Login</v-btn>
-          </v-col>
-          <v-cols cols="auto">
-            <v-btn @click="$router.push('/register')" rounded="xl" color="#9A00DA" class="title2">Register</v-btn>
-          </v-cols>
-        </template>
-        <template v-else>
-          <v-avatar class="ma-2" size="50" >
-            <v-img @click="$router.push('/profile')" :src="userData.img" alt="Avatar"></v-img>
-          </v-avatar>
-          <v-btn text @click="$router.push('/setting/account')" class="title2">@{{ userData.pseudo }}</v-btn >
-          <v-col cols="auto">
-            <v-btn text @click="logout" rounded="xl" class="ma-2 title2">Logout</v-btn>
-          </v-col>
-        </template>
-    </v-toolbar>
-    <v-spacer></v-spacer>
-
-    <v-row>
-      <v-spacer></v-spacer>
-      <v-card-title class="title3">WELCOME TO V'S ALLURE</v-card-title>
-      <v-spacer></v-spacer>
-    </v-row>
-    <v-row>
-      <v-spacer></v-spacer>
-      <v-card-subtitle class="title4">Unveil your radiance, embrace your allure</v-card-subtitle>
-      <v-spacer></v-spacer>
-    </v-row>
-    <v-spacer></v-spacer>
-
-    <v-divider thickness="70"></v-divider>
-
+      </v-toolbar>
   </v-col>
 </template>
 
